e647da385e6704a9426857a1067d685c0de7388b
Marcus Vinicius Vieira Badiale <marcus.badiale@gmail.com>
2020-04-07 16:30:59 -0300

    adding final file

--- UnwindApp/UnwindApp/UserFlow/FeelingsScreen.swift
+++ UnwindApp/UnwindApp/UserFlow/FeelingsScreen.swift
@@ -8,21 +8,62 @@
 
 import SwiftUI
 
+//0,07
+
+extension Color {
+    static let lightGreen = Color(red: 245 / 255, green: 255 / 255, blue: 246 / 255)
+    static let strokeGreen = Color(red: 132 / 255, green: 219 / 255, blue: 49 / 255)
+    static let strokeGray = Color(red: 217/255, green: 219/255, blue: 226/255)
+    static let fontColorBlue = Color(red: 81/255, green: 154/255, blue: 255/255)
+    static let fontColorBlack = Color(red: 37/255, green: 46/255, blue: 72/255)
+}
+
+class FeelingsViewModel: ObservableObject {
+    
+    @Published var feelingName = ""
+    @Published var feelingImageName = "default"
+    @Published var selected = ""
+    
+    let array: [[String]] = [
+        ["Muito Feliz", "Feliz", "Neutro"],
+        ["Triste", "Muito Triste", "Outro"]
+    ]
+    
+    func changeImage(imageName: String) {
+        self.feelingImageName = imageName
+    }
+    
+    func changeName(feelingName: String) {
+        self.feelingName = feelingName
+    }
+}
+
+
 struct FeelingsScreen: View {
     
-    let array = ["Muito Feliz", "Feliz", "Neutro"]
+    @ObservedObject var viewModel = FeelingsViewModel()
     
     var body: some View {
         VStack(spacing: 50){
             VStack{
-                Text("Julia, como você está se sentindo?")
-                Image("default")
-                Text("Muito Feliz")
-                    .font(.system(size: 12))
+                HStack{
+                   Text("Julia, como você está se")
+                    .font(.system(size: 40))
+                    .foregroundColor(Color.fontColorBlack)
+                    Text("sentindo?")
+                    .font(.system(size: 40)).bold()
+                    .foregroundColor(Color.fontColorBlack)
+                }
+                Image(viewModel.feelingImageName)
+                Text(viewModel.feelingName)
+                    .font(.system(size: 30))
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.fontColorBlue)
             }
             VStack{
-                SelectFeelings(array: array)
-                SelectFeelings(array: array)
+                ForEach(viewModel.array, id: \.self) { row in
+                    SelectFeelings(array: row, viewModel: self.viewModel)
+                }
             }
         }
     }
@@ -34,28 +75,57 @@ struct FeelingsScreen_Previews: PreviewProvider {
     }
 }
 
+struct FeelingButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+        .padding(.horizontal, 75)
+        .padding(.vertical, 35)
+        .contentShape(RoundedRectangle(cornerRadius: 8))
+    }
+}
+
 struct SelectFeelings: View {
     
     @State var array: [String]
+    @ObservedObject var viewModel: FeelingsViewModel
     
     var body: some View {
         HStack(spacing: 10){
-            ForEach(array, id: \.self) { teste in
+            ForEach(array, id: \.self) { feeling in
                 Button(action: {
-                    print("apertou")
+                    self.viewModel.selected = feeling
+                    self.viewModel.changeImage(imageName: "img_big")
+                    self.viewModel.changeName(feelingName: feeling)
                 }) {
                     VStack{
-                        Image("default")
+                        Image("img_")
                             .resizable()
                             .frame(width: self.feelingSize(isWidth: true), height: self.feelingSize(isWidth: false))
-                        Text(teste)
-                    }.padding(.horizontal, 30)
-                        .padding(.vertical, 20)
-                        .overlay(
+                        Text(feeling)
+                            .font(.system(size: 20))
+                            .fontWeight(.medium)
+                            .foregroundColor(Color.fontColorBlue)
+                    }
+                }
+                .background(
+                    Group{
+                        if self.viewModel.selected == feeling {
+                            Color.lightGreen
+                        }
+                    }
+                )
+                .overlay(
+                    Group{
+                        if self.viewModel.selected == feeling {
                             RoundedRectangle(cornerRadius: 8)
-                                .stroke(Color(.systemGray3), lineWidth: 1)
-                        )
-                }.buttonStyle(PlainButtonStyle())
+                                .stroke(Color.strokeGreen, lineWidth: 5)
+                        } else {
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.strokeGray, lineWidth: 2)
+                        }
+                    }
+                )
+                .buttonStyle(FeelingButtonStyle())
             }
         }
     }
@@ -65,9 +135,9 @@ struct SelectFeelings: View {
         var size: CGFloat
         
         if isWidth{
-            size = UIScreen.main.bounds.width / 5
+            size = UIScreen.main.bounds.width * 0.125
         } else {
-            size = UIScreen.main.bounds.width / 6
+            size = UIScreen.main.bounds.width * 0.11
         }
         return size
     }
